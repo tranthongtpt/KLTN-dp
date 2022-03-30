ed1f92a1c314aed9159fe910e50e29c6d9468ff9
tranthongtpt <tranthongtpt@gmail.com>
2022-03-30 14:57:31 +0700

    fisrt time 30-3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,3 @@
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
-
-function register() {
-    x.style.left = "-550px";
-    y.style.left = "55px";
-    z.style.left = "110px";
-}
-function login() {
-    x.style.left = "33px";
-    y.style.left = "500px";
-    z.style.left = "0";
-}
 
 var x=true;
 function myfunction(){
@@ -24,27 +10,6 @@ function myfunction(){
   }
 }
 
-
-let loginForm = document.querySelector('.login-form-container');
-
-document.querySelector('#login-btn').onclick = () =>{
-  loginForm.classList.toggle('active');
-}
-
-document.querySelector('#close-login-btn').onclick = () =>{
-  loginForm.classList.remove('active');
-}
-
-let signForm = document.querySelector('.sign-form-container');
-
-document.querySelector('#sign-btn').onclick = () =>{
-  signForm.classList.toggle('active');
-}
-
-document.querySelector('#close-sign-btn').onclick = () =>{
-  signForm.classList.remove('active');
-}
-
 window.onscroll = () =>{
 
   if(window.scrollY > 80){
@@ -54,7 +19,6 @@ window.onscroll = () =>{
   }
 
 }
-
 window.onload = () =>{
 
   if(window.scrollY > 80){
@@ -65,55 +29,49 @@ window.onload = () =>{
 
 }
 
-var swiper = new Swiper(".drug-slider", {
+let slides = document.querySelectorAll('.home .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+var swiper = new Swiper(".featured-slider", {
+  spaceBetween: 10,
   loop:true,
   centeredSlides: true,
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   breakpoints: {
     0: {
       slidesPerView: 1,
     },
-    768: {
+    450: {
       slidesPerView: 2,
     },
-    1024: {
+    768: {
       slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
     },
   },
 });
-var swiper = new Swiper('.swiper-container', {
-	loop:true,
-  centeredSlides: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-	breakpoints: {
-	  620: {
-		slidesPerView: 1,
-		spaceBetween: 20,
-	  },
-	  680: {
-		slidesPerView: 2,
-		spaceBetween: 40,
-	  },
-	  920: {
-		slidesPerView: 3,
-		spaceBetween: 40,
-	  },
-	  1240: {
-		slidesPerView: 4,
-		spaceBetween: 50,
-	  },
-	} 
-    });
-
-var swiper = new Swiper(".reviews-slider", {
-  spaceBetween: 10,
-  grabCursor:true,
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 20,
   loop:true,
   centeredSlides: true,
   autoplay: {
